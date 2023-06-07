c4c30f52bd5690113238384e8045fa5cb4d87095
Ulric Qin <ulric.qin@gmail.com>
2023-06-07 11:51:17 +0800

    code refactor

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+export GOPROXY=https://goproxy.cn
+
+if ! go build; then
+    echo "build failed"
+    exit 1
+fi
+
+version=$(./catpaw -v)
+echo "version: $version"
+
+mkdir -p dist/catpaw-v${version}-linux-amd64
+
+cp catpaw dist/catpaw-v${version}-linux-amd64/
+cp -r conf.d dist/catpaw-v${version}-linux-amd64/
+cp -r scripts dist/catpaw-v${version}-linux-amd64/
+
+cd dist
+tar -zcvf catpaw-v${version}-linux-amd64.tar.gz catpaw-v${version}-linux-amd64
+
+echo "build success"
