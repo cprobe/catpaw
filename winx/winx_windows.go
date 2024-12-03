75bfc149f854fd3ff79bb8a9899650936dcd7799
Ulric Qin <ulric.qin@gmail.com>
2024-12-03 20:12:21 +0800

    fix win-service-name default settings

--- winx/winx_windows.go
+++ winx/winx_windows.go
@@ -11,8 +11,8 @@ import (
 )
 
 var (
-	flagWinSvcName      = flag.String("win-service-name", "categraf", "Set windows service name")
-	flagWinSvcDesc      = flag.String("win-service-desc", "Categraf", "Set windows service description")
+	flagWinSvcName      = flag.String("win-service-name", "catpaw", "Set windows service name")
+	flagWinSvcDesc      = flag.String("win-service-desc", "catpaw is a monitoring tool", "Set windows service description")
 	flagWinSvcInstall   = flag.Bool("win-service-install", false, "Install windows service")
 	flagWinSvcUninstall = flag.Bool("win-service-uninstall", false, "Uninstall windows service")
 	flagWinSvcStart     = flag.Bool("win-service-start", false, "Start windows service")
